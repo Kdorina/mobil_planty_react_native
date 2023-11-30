c717c3a8dec382ebcc7bc2a4d0a0fa50ba17ac9d
Kdorina <kovacsdorina2@gmail.com>
2023-11-30 19:13:03 +0100

    planty app első screen kész

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LinearGradient start={{x: 0, y: 1}} end={{x: 1, y: 0}} colors={['#54c0cc','#dcd964',]} style={styles.container}>
+      <Image source={require('./assets/leaf.png')} style={{ justifyContent: 'center',width:350,resizeMode: 'contain'}}/>
+      <Text style={styles.mainText}>Planty</Text>
+      <Text style={styles.miniText}>Gondozd növényeidet,</Text>
+      <Text style={styles.miniText}>követsd nyomon egészségüket</Text>
+
+      <View style={styles.bgButton}>
+        <View style={{backgroundColor:'#ffff', padding:10, borderRadius:50, width:200}}>
+          <Button title='Tovább' color='#54c0cc' onPress={() => Alert.alert('Button pressed')}></Button>
+        </View>
+      </View>
       <StatusBar style="auto" />
+      </LinearGradient>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex:1,
+    width:'100%',
+   /*  alignItems: 'center', */
+  /*   justifyContent: 'center', */
+  },
+  mainText:{
+    textAlign:'left',
+    marginLeft:20,
+    fontWeight:"bold",
+    fontSize:100,
+  },
+  miniText:{
+    marginLeft:20,
+    fontSize:20,
+  },
+  bgButton:{
+    flex:1,
+    width:'100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
