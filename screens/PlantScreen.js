95b6c23afe72109a43721d8ebaa0106ee2e6db66
Kdorina <kovacsdorina2@gmail.com>
2024-01-23 18:40:32 +0100

    profilScreen formázása

--- screens/PlantScreen.js
+++ screens/PlantScreen.js
@@ -1,48 +1,16 @@
 import { View, Text, SafeAreaView, Button, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import {
-    LineChart,
-    BarChart,
-    PieChart,
-    ProgressChart,
-    ContributionGraph,
-    StackedBarChart
-  } from "react-native-chart-kit";
-  import { Dimensions } from "react-native";
+
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faDroplet, faSun, faTemperature0, faTemperature1, faTemperature3, faTemperature4, faTemperatureArrowDown } from '@fortawesome/free-solid-svg-icons';
 import ProgressCircle from 'react-native-progress-circle'
 import { faPagelines } from '@fortawesome/free-brands-svg-icons';
 import BottomNavigation from '../navigation/BottomNavigation';
 
-  const screenWidth = Dimensions.get("window").width;
-  const chartConfig = {
-    backgroundGradientFrom: "#1E2923",
-    backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: "#08130D",
-    backgroundGradientToOpacity: 0,
-    color: (opacity = 1) => `rgba(255, 121, 25, ${opacity})`,
-    strokeWidth: 1, // optional, default 3
-    barPercentage: 0.5,
-    useShadowColorFromDataset: false // optional
-  };
 
 export default function PlantScreen() {
     const navigation = useNavigation(); 
-
-    const water = {
-       /*  labels: ["Víz"], // optional */
-        data: [0.8]
-      };
-      const light = {
-       /*  labels: ["Fény"], // optional */
-        data: [0.5]
-      };
-      const nutirent = {
-       /*  labels: ["Tápanyag"], // optional */
-        data: [0.2]
-      };
 
   return (
         /* #1E2923 ez a szín jólnézne ki hátttérnek */
@@ -62,42 +30,7 @@ export default function PlantScreen() {
           <Text style={{fontWeight:'bold',fontSize:17}}>Fény</Text>
           <Text style={{fontSize:30}}><FontAwesomeIcon size={25} icon={faSun}/> 50%</Text>
          </View>
-           {/*  <View style={{marginLeft:120,width:200, height:400, justifyContent:'center', alignItems:'center'}}>
-                <View style={{backgroundColor:'#FDFFAB', width:130, height:100,margin:10, borderRadius:20}}>
-                <ProgressChart style={{marginHorizontal:-120, marginVertical:-10}}
-                data={water}
-                width={screenWidth}
-                height={120}
-                strokeWidth={10}
-                radius={40}
-                chartConfig={chartConfig}
-                hideLegend={false}
-                />
-                </View>
-                <View style={{backgroundColor:'#FDFFAB', width:130, height:100,margin:10, borderRadius:20}}>
-                <ProgressChart style={{marginHorizontal:-120, marginVertical:-10}}
-                data={nutirent}
-                width={screenWidth}
-                height={120}
-                strokeWidth={10}
-                radius={40}
-                chartConfig={chartConfig}
-                hideLegend={false}
-                />
-                </View>
-                <View style={{backgroundColor:'#FDFFAB', width:130, height:100,margin:10, borderRadius:20}}>
-                <ProgressChart style={{marginHorizontal:-120, marginVertical:-10}}
-                data={light}
-                width={screenWidth}
-                height={120}
-                strokeWidth={10}
-                radius={40}
-                chartConfig={chartConfig}
-                hideLegend={false}
-                
-                />
-                </View>
-            </View> */}
+        
         </View>
 
         <View style={{backgroundColor:'white', zIndex:1, flex:1 ,marginVertical:-70, borderRadius:80}}>
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,48 +1,65 @@
 import { View, Text, SafeAreaView, StyleSheet, Button, TouchableOpacity} from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function ProfileScreen() {
   const navigation = useNavigation()
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.holder}>
 
-    <View style={styles.profileBox}>
-      <Text style={styles.mainText} >Profil</Text>
-      <TouchableOpacity onPress={()=> navigation.navigate('')}>
-        <View style={{backgroundColor:'grey',borderRadius:'50%'}}>
-        <Text style={{width:100,height:100}}></Text>
-        </View>
-      </TouchableOpacity>
-
-      <View style={styles.optionsHolder}>
-      <TouchableOpacity onPress={()=> navigation.navigate('')}>
-        <View style={{backgroundColor:'#ff7919',borderRadius:'50%',margin:10}}>
-        <Text style={{width:70,height:70}}></Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={()=> navigation.navigate('')}>
-        <View style={{backgroundColor:'#ff7919',borderRadius:'50%',margin:10}}>
-        <Text style={{width:70,height:70}}></Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={()=> navigation.navigate('')}>
-        <View style={{backgroundColor:'#ff7919',borderRadius:'50%',margin:10}}>
-        <Text style={{width:70,height:70,}}></Text>
-        </View>
-      </TouchableOpacity>
-      </View>
-
+    <View style={{margin:30}}>
+      <Text style={{color:'grey', fontSize:15}}  onPress={()=> navigation.navigate('Home')}><FontAwesomeIcon size={40} icon={faChevronCircleLeft}/></Text>
     </View>
 
-    <View style={styles.profileDatas}>
-          <Text style={styles.dataText}>Név</Text>
-          <View style={styles.datas}><Text style={{fontSize:20,}}>Kovács Piroska</Text></View>
-          <Text style={styles.dataText}>Email</Text>
-          <View style={styles.datas}><Text style={{fontSize:20,}}>primiri@gmail.com</Text></View>
-          <Text style={styles.dataText}>Növényeid száma: 3</Text>
+    <View style={styles.container}>
+    <View style={styles.profileBox}>
+      <Text style={styles.mainText}>Profil</Text>
+    </View>
 
-      <View >
+    <View style={{backgroundColor:'#fafafa', width:'90%',
+                padding:20, borderRadius:15,
+                marginBottom:10,flexDirection:'column', 
+                shadowColor: "#B3E283", shadowOpacity:  0.89,
+                shadowOffset: {
+                  width: 0,
+                  height: 4,
+                }, 
+                shadowRadius: 10.62, 
+                elevation: 6}}>
+                <Text style={{fontSize:20, fontWeight:'700'}}>Név</Text>
+                <Text style={{fontSize:15}}>Herbáli Andris</Text>
+    </View>
+    <View style={{backgroundColor:'#fafafa', width:'90%',
+                padding:20, borderRadius:15,
+                marginBottom:10,flexDirection:'column', 
+                shadowColor: "#B3E283", shadowOpacity:  0.89,
+                shadowOffset: {
+                  width: 0,
+                  height: 4,
+                }, 
+                shadowRadius: 10.62, 
+                elevation: 6}}>
+                <Text style={{fontSize:20, fontWeight:'700'}}>Email</Text>
+                <Text style={{fontSize:15}}>herbali@gmail.com</Text>
+    </View>
+    <View style={{backgroundColor:'#fafafa', width:'90%',
+                padding:20, borderRadius:15,
+                marginBottom:10,flexDirection:'column', 
+                shadowColor: "#B3E283", shadowOpacity:  0.89,
+                shadowOffset: {
+                  width: 0,
+                  height: 4,
+                }, 
+                shadowRadius: 10.62, 
+                elevation: 6}}>
+                <Text style={{fontSize:20, fontWeight:'700'}}>Csatlakozás</Text>
+                <Text style={{fontSize:15}}>2023.11.12</Text>
+    </View>
+              
+
+      <View style={{width:'80%', marginTop:10}}>
           <View style={{flexDirection:"row", alignItems:'center', backgroundColor:"#DBFFAD", borderRadius:10, marginBottom:10}}>
             <View style={{backgroundColor:'#a9e048',borderRadius:'10%',margin:10,width:40,height:40}}></View>
             <Text style={styles.myplant}>Kaktusz</Text>
@@ -69,8 +86,14 @@ export default function ProfileScreen() {
   )
 }
 const styles = StyleSheet.create({
-  container: {
+  holder: {
     flex:1,
+    width:'100%',
+    height:'100%',
+    backgroundColor:'white',
+  /*   justifyContent: 'center', */
+  },
+  container:{
     width:'100%',
     height:'100%',
     alignItems: 'center',
@@ -85,28 +108,6 @@ const styles = StyleSheet.create({
     marginBottom:20,
     fontWeight:"bold",
     fontSize:25,
-  },
-  optionsHolder:{
-    marginTop:25,
-    flexDirection:'row'
-  },
-  profileDatas:{
-    marginTop:20,
-    padding:10,
-    backgroundColor:'white',
-    height:'55%',
-    width:'80%',
-    borderRadius:20,
-  },
-  dataText:{
-    fontSize:20,
-    fontWeight:'bold',
-   
-  },
-  datas:{
-    backgroundColor:"#DBFFAD",
-    borderRadius:5,
-    padding:5,
   },
 
 });
