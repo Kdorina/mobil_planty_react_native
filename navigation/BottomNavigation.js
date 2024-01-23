bc29d67c2bbd8b9a608509d588b36f19be19cc39
Kdorina <kovacsdorina2@gmail.com>
2024-01-23 20:27:40 +0100

    navigacios sav formázása/ hover hasznalataval

--- navigation/BottomNavigation.js
+++ navigation/BottomNavigation.js
@@ -1,8 +1,9 @@
-import { View, Text,StyleSheet} from 'react-native'
+import { View, Text,StyleSheet, TouchableOpacity, Pressable} from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faHouse, faMugSaucer, faPeopleLine, faPersonRifle, faPlantWilt, faPortrait, faSeedling, faUser } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function BottomNavigation() {
   const navigation = useNavigation()
@@ -12,9 +13,9 @@ export default function BottomNavigation() {
     <View style={{ alignItems:'center',justifyContent:'center', height:100}}>
         <View style={{backgroundColor:"white",alignItems:'center', justifyContent:'center',width:300, height:70, borderRadius:'50%'}}>
             <View style={styles.container}>
-            <Text style={styles.icons} onPress={()=> navigation.navigate('Plants')}><FontAwesomeIcon size={25} icon={ faSeedling } color={ 'black' } /></Text>
-            <Text style={styles.icons} onPress={()=> navigation.navigate('Home')}><FontAwesomeIcon size={25}  icon={ faHouse } color={ 'black' } /></Text>
-            <Text style={styles.icons} onPress={()=> navigation.navigate('Profile')}><FontAwesomeIcon size={25}  icon={ faUser } color={ 'black'} /></Text>
+            <Pressable style={({pressed}) => [ pressed && {opacity:0.8, backgroundColor:'#D9EDBF', padding:10, borderRadius:'50%'},]} onPress={()=> navigation.navigate('Plants')}><FontAwesomeIcon size={25}  icon={ faSeedling } color={ 'black'} /></Pressable>
+            <Pressable style={({pressed}) => [ pressed && {opacity:0.8, backgroundColor:'#D9EDBF', padding:10, borderRadius:'50%'},]} onPress={()=> navigation.navigate('Home')}><FontAwesomeIcon size={25}  icon={ faHouse } color={ 'black'} /></Pressable>
+            <Pressable style={({pressed}) => [ pressed && {opacity:.8, backgroundColor:'#D9EDBF', padding:10, borderRadius:'50%'},]} onPress={()=> navigation.navigate('Profile')}><FontAwesomeIcon size={25}  icon={ faUser } color={ 'black'} /></Pressable>
             </View>
         </View>
     </View>
